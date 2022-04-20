978144e21eb5253634a65095194bb6e244ff9c78
Daehyun Paik <paik@a30a.dev>
2022-04-21 00:20:51 +0900

    Add a skeleton of cli

---
+++ src/bootstrap.ts
@@ -0,0 +1,31 @@
+import { ArgumentParser } from 'argparse';
+
+const version = '0.0.1';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function initCommands(): ArgumentParser {
+    const parser = new ArgumentParser({
+        description: 'hardhat network contoller (hnctl)',
+        add_help: true,
+    });
+
+    parser.add_argument('-v', '--version', { action: 'version', version });
+
+    const subParsers = parser.add_subparsers({ dest: 'subcommand' });
+    subParsers.add_parser('start', {
+        help: 'start hardhat network',
+    });
+    subParsers.add_parser('restart', {
+        help: 'restart hardhat network',
+    });
+    subParsers.add_parser('destroy', {
+        help: 'destroy hardhat network',
+    });
+    subParsers.add_parser('logs', {
+        help: 'print logs of hardhat network',
+    });
+
+    return parser
+}
+
+export {initCommands}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-const message = "hello"
+import {initCommands} from  './bootstrap.js'
 
-console.log(message)
-`
+const parser = initCommands()
+// TODO: Move to bootstrap.ts
+const args = parser.parse_args()
+const subCommand = args.subcommand;
+
+switch (subCommand) {
+    case 'start':
+        console.log('Implement me!');
+        break;
+    case 'restart':
+        console.log('Implement me!');
+        break;
+    case 'destroy':
+        console.log('Implement me!');
+        break;
+    case 'logs':
+        console.log('Implement me!');
+        break;
+    case 'status':
+        console.log('Implement me!');
+        break;
+    default:
+        parser.print_help();
+}
